62727232f909187e8493bd8c8892e7b244cd5074
Rômulo Ciro <romulo.ciro@hotmail.com>
2020-12-29 14:23:16 -0300

    Refactoring accepting completing old tasks

--- src/middlewares/TaskValidation.js
+++ src/middlewares/TaskValidation.js
@@ -15,10 +15,34 @@ const TaskValidation = async (req, res, next) => {
     return res.status(400).json({ error: 'descrição é obrigatório'})
     else if(!when)
     return res.status(400).json({ error: 'Data e Hora são obrigatórios'})
-    else if(isPast(new Date(when)))
-    return res.status(400).json({ error: 'Escolha uma data e hora futura'})
+    
     else{
-        next()
+        let exists;
+
+        if(req.params.id){
+            exists = await TaskModel.
+                        findOne({
+                            '_id': {'$ne': req.params.id},
+                            'when': {'$eq': new Date(when) },
+                            'macaddress': {'$in': macaddress }
+                        });
+        }else{
+            if(isPast(new Date(when)))
+                return res.status(400).json({ error: 'Escolha uma data e hora futura'})
+            exists = await TaskModel.
+            findOne(
+                {
+                    'when': {'$eq': new Date(when) },
+                    'macaddress': {'$in': macaddress }
+
+                });
+        }
+
+        if(exists){
+            return res.status(400).json({ error: 'Já existe uma tarefa nesse dia e horário'})
+        }
+
+        next();
     }
 }
 
